ec1a049cb576afa2e3dc9efa61dce112a27f5204
noammiz007 <31467235+noammiz007@users.noreply.github.com>
2018-09-17 01:54:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,49 +5,60 @@ const app = express();
 app.listen(8080);
 
 setInterval(() => {
-http.get('${process.env.bot1}'`);
+http.get(`${process.env.bot1}`);
 }, 10);
 
-setInterval(() => {
-http.get('${process.env.bot2}'`);
-}, 10);
 
 setInterval(() => {
-http.get('${process.env.bot3}'`);
+http.get(`${process.env.bot2}`);
 }, 10);
 
-setInterval(() => {
-http.get('${process.env.bot4}'`);
-}, 10);
 
 setInterval(() => {
-http.get('${process.env.bot5}'`);
+http.get(`${process.env.bot3}`);
 }, 10);
 
-setInterval(() => {
-http.get('${process.env.bot6}'`);
-}, 10);
 
 setInterval(() => {
-http.get('${process.env.bot7}'`);
+http.get(`${process.env.bot4}`);
 }, 10);
 
-setInterval(() => {
-http.get('${process.env.bot8}'`);
-}, 10);
 
 setInterval(() => {
-http.get('${process.env.bot9}'`);
+http.get(`${process.env.bot5}`);
 }, 10);
 
-setInterval(() => {
-http.get('${process.env.bot10}'`);
-}, 10);
 
 setInterval(() => {
-http.get('${process.env.bot11}'`);
+http.get(`${process.env.bot6}`);
 }, 10);
 
+
 setInterval(() => {
-http.get('${process.env.bot12}'`);
+http.get(`${process.env.bot7}`);
+}, 10);
+
+
+setInterval(() => {
+http.get(`${process.env.bot8}`);
+}, 10);
+
+
+setInterval(() => {
+http.get(`${process.env.bot9}`);
+}, 10);
+
+
+setInterval(() => {
+http.get(`${process.env.bot10}`);
+}, 10);
+
+
+setInterval(() => {
+http.get(`${process.env.bot11}`);
+}, 10);
+
+
+setInterval(() => {
+http.get(`${process.env.bot12}`);
 }, 10);
