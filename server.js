dfba3ed8efec0bc1eb4c4e32b07c3cadb5b51606
noammiz007 <31467235+noammiz007@users.noreply.github.com>
2018-09-18 14:29:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,59 +6,59 @@ app.listen(8080);
 
 setInterval(() => {
 http.get(`${process.env.bot1}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot2}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot3}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot4}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot5}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot6}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot7}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot8}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot9}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot10}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot11}`);
-}, 10);
+}, 120000);
 
 
 setInterval(() => {
 http.get(`${process.env.bot12}`);
-}, 10);
+}, 120000);
